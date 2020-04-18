8d4c2fab576a05ebc01fc3cea5cbf5fe635a5c8b
luoyifan <luoyifan@qq.com>
2020-04-18 12:03:44 +0800

    解决 ts 版本问题

--- src/CtlUserBase.ts
+++ src/CtlUserBase.ts
@@ -1,10 +1,10 @@
-import * as YvanUI from './index'
+// import * as YvanUI from './index'
 
-// 自定义组件
-@YvanUI.UserComponent('my_component')
-export abstract class UserComponentBase<T> {
-  /**
-   * 执行渲染
-   */
-  abstract render(parentElement: Element): void
-}
+// // 自定义组件
+// @YvanUI.UserComponent('my_component')
+// export abstract class UserComponentBase<T> {
+//   /**
+//    * 执行渲染
+//    */
+//   abstract render(parentElement: Element): void
+// }
--- src/YvanRender.ts
+++ src/YvanRender.ts
@@ -1,19 +1,3 @@
-export function f1() {
-  console.log('this is yvan-ui.f1')
-}
-
-export function f2() {
-  console.log('this is yvan-ui.f2')
-}
-
-export function f3() {
-  console.log('this is yvan-ui.f3')
-}
-
-export function f4() {
-  console.log('this is yvan-ui.f4')
-}
-
 /* eslint-disable */
 import Vue from 'vue'
 import { BaseModule, BizWatch, VJson } from './YvanUIModule'
