eb798dbb38fe3b87bb8b56fc539db41593a3bcc7
luoyifan <luoyifan@qq.com>
2020-04-20 19:53:44 +0800

    ctlGrid pagination 情况下 autoLoad 不生效的问题

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,5 +2,5 @@ import es from './rollup.config.es'
 import dev from './rollup.config.dev'
 import prod from './rollup.config.prod'
 
-export default [es, dev, prod]
-// export default [dev]
+// export default [es, dev, prod]
+export default [dev]
